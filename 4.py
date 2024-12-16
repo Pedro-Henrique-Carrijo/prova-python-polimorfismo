efa2a51bdd8d99aac72456a819dc623dc6dec833
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2024-12-16 19:53:34 -0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -6,3 +6,32 @@ mover().
 19. Explique como a herança facilita a implementação do polimorfismo nesse exemplo.
 20. Adicione outra classe chamada Patinete que também herda de Veiculo, mas com um método
 mover() que imprime "Patinetes deslizam".'''
+# 19 facilita implementação de polimorfismo, permitindo comportamentos específicos.
+class Veiculo:
+ def mover(self):
+    pass
+
+
+class Carro(Veiculo):
+ def mover(self):
+    print("Carro em movimento.")
+
+class Moto(Veiculo):
+ def mover(self):
+    print("Moto em movimento.")
+
+class Caminhão(Veiculo):
+ def mover(self):
+    print("Caminhão em movimento.")
+
+
+veículos = [Carro(), Moto(), Caminhão()]
+for veiculo in veículos:
+ veiculo.mover()
+
+class Patinete(Veiculo):
+ def mover(self):
+    print("Patinete deslizando.")
+
+patinete = Patinete()
+patinete.mover() 
