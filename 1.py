597b289b1cb8f35a6ff290d19cc433b6b08fa101
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2024-12-16 19:34:06 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,3 +5,30 @@
 deles.
 4. Crie dois objetos, um de cada classe, e chame o método executar() de ambos.
 5. Explique o que acontece ao tentar chamar o método executar() sem implementá-lo.'''
+
+# polimorfismo é um conceito fundamental da programação e se refere à capacidade de objetos de diferentes classes responderem de maneiras diferentes à mesma mensagem
+
+class Pessoa:
+ pass
+
+class Objeto:
+ pass
+
+
+class Pessoa:
+ def executar(self):
+ pass
+
+class Objeto:
+ def executar(self):
+ pass
+
+pessoa = Pessoa()
+objeto = Objeto()
+
+pessoa.executar()  
+objeto.executar()  
+
+# 5. Explicação
+# se você chamar o executar sem implementação é como se ele não existisse na classe
+
