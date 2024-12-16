b0fe067c526166ceed9dc80e6210195b82cad87a
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2024-12-16 19:49:05 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,8 @@
+''' Parte 4: Polimorfismo com Herança
+16. Crie uma classe base chamada Veiculo com um método mover() vazio (usando pass).
+17. Faça as classes Carro, Moto e Caminhão herdarem de Veiculo e sobrescrevam o método
+mover().
+18. Crie uma lista contendo objetos de cada classe e itere sobre ela chamando o método mover().
+19. Explique como a herança facilita a implementação do polimorfismo nesse exemplo.
+20. Adicione outra classe chamada Patinete que também herda de Veiculo, mas com um método
+mover() que imprime "Patinetes deslizam".'''
