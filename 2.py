0e747d089ebf3d844456eeb5db5c645718569846
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2024-12-16 19:38:07 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,30 @@
+'' Parte 2: Polimorfismo com Métodos
+6. Crie uma classe chamada Leão com um método falar() que imprime "Rugido!".
+7. Crie outra classe chamada Lobo com o método falar() que imprime "Uivo!".
+8. Crie objetos para ambas as classes e chame o método falar() de cada um.
+9. O que acontece quando dois objetos diferentes possuem métodos com o mesmo nome?
+10. Implemente uma classe Arara com o método falar() que imprime "Bom dia!" e repita o
+processo.'''
+
+class Leão:
+ def falar(self):
+ print("Rugido!")
+
+
+class Lobo:
+ def falar(self):
+ print("Uivo!")
+
+leão = Leão()
+lobo = Lobo()
+
+leão.falar()
+lobo.falar()
+
+
+class Arara:
+ def falar(self):
+ print("Bom dia!")
+
+arara = Arara()
+arara.falar()
