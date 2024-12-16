3613fd8273eba3c84b8ddbf150083486318838a2
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2024-12-16 19:18:56 -0300

    Update and rename 1 to 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
-# Parte 1: Entendendo o Polimorfismo
+''' Parte 1: Entendendo o Polimorfismo
 1. Explique com suas palavras o que é polimorfismo.
 2. Crie duas classes vazias chamadas Pessoa e Objeto.
 3. Adicione um método chamado executar() em ambas as classes, mas sem implementar nada dentro
 deles.
 4. Crie dois objetos, um de cada classe, e chame o método executar() de ambos.
-5. Explique o que acontece ao tentar chamar o método executar() sem implementá-lo.
+5. Explique o que acontece ao tentar chamar o método executar() sem implementá-lo.'''
